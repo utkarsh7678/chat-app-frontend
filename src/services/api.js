0f85b16d85acca3c9bc0ab571ceb9799565a1bb4
utkarsh7678 <139717056+utkarsh7678@users.noreply.github.com>
2025-07-24 04:54:07 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -71,7 +71,7 @@ export const user = {
     const token = useStore.getState().token;
     return api.put('/api/user/avatar', formData, {
       headers: {
-        'Content-Type': 'multipart/form-data',
+        //'Content-Type': 'multipart/form-data',
         ...(token && { Authorization: `Bearer ${token}` })
       }
     });
